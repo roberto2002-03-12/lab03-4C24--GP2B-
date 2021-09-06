f4e9628df1d603c1b568ed024543df9763a7eaef
Emilio Contreras Gonzales <emilio.contreras@tecsup.edu.pe>
2021-09-06 16:04:09 -0500

    Primer commit

---
+++ src/main/java/com/tecsup/lab03/lab03_4C24__GP2B_/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab03.lab03_4C24__GP2B_;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/tecsup/lab03/lab03_4C24__GP2B_/AppTest.java
@@ -0,0 +1,20 @@
+package com.tecsup.lab03.lab03_4C24__GP2B_;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
